92b3f69c3245cc8d25f19a4a54b569fed24ce2c9
Yakoob_01 <100505972+Yakoob721@users.noreply.github.com>
2026-01-08 18:26:01 +0530

    Add: ViPER4AndroidFX

--- device.mk
+++ device.mk
@@ -378,3 +378,6 @@ PRODUCT_COPY_FILES += \
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/fleur/fleur-vendor.mk)
+
+# ViPER4AndroidFX
+$(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
